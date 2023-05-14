2496d7e3b593330e78ba6d627d0b835861f10518
anwb <antonwb@live.dk>
2023-05-14 14:15:47 +0200

    variable naming

--- heatmapAll/scatterPlotValenceDanceabilityAllTwo.py
+++ heatmapAll/scatterPlotValenceDanceabilityAllTwo.py
@@ -13,19 +13,22 @@ alt.data_transformers.disable_max_rows()
 
 # Load dataframe
 
+import pandas as pd
+
+# Load the dataset from the csv file
 df_genres_clean_2 = pd.read_csv("data/mergedStreaming/GenresMergedStreaming.csv")
 
 # Remove "No genre information available"
 df_genres_clean = df_genres_clean_2[df_genres_clean_2['artistGenres'] != 'No genre information available']
 
-# Count the occurrences of each genre and get the top 40 most common genres
-top_genres = df_genres_clean_2['artistGenres'].value_counts().nlargest(10).index.tolist()
+# Count the occurrences of each genre and get the top 10 most common genres
+top_genres = df_genres_clean['artistGenres'].value_counts().nlargest(10).index.tolist()
 
-# Filter the DataFrame to only include rows with one of the top 40 genres
-df_genres_clean_40 = df_genres_clean_2[df_genres_clean_2['artistGenres'].isin(top_genres)]
+# Filter the DataFrame to only include rows with one of the top 10 genres
+df_genres_clean_10 = df_genres_clean[df_genres_clean['artistGenres'].isin(top_genres)]
 
 # Create the strip plot using the filtered DataFrame
-scatter_plot_valence_danceability_all = alt.Chart(df_genres_clean_40).mark_circle(size=30, color="#6f82ac").encode(
+scatter_plot_valence_danceability_all = alt.Chart(df_genres_clean_10).mark_circle(size=30, color="#6f82ac").encode(
     y=alt.Y("danceability:Q", axis=alt.Axis(title="Danceability")),
     x=alt.X("valenceScore:Q", axis=alt.Axis(title="Valence Score")),
     tooltip=['trackName:N', "artistName:N", 'artistGenres:N','valenceScore:Q', 'danceability:Q', ],
