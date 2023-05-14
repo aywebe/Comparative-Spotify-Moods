d2ba3790413795f2943b8f10c6aad0590d8025ad
anwb <antonwb@live.dk>
2023-05-14 13:13:06 +0200

    rewind

--- stripPlotAll/stripPlotTopTenGenresValenceAll.py
+++ stripPlotAll/stripPlotTopTenGenresValenceAll.py
@@ -26,28 +26,17 @@ df_genres_clean_40 = df_genres_clean[df_genres_clean['artistGenres'].isin(top_ge
 
 # Create the strip plot using the filtered DataFrame
 strip_plot_valence_artist_genres_all = alt.Chart(df_genres_clean_40).mark_circle(size=30, color="#5c3a94" , opacity=1.0).encode(
-    y=alt.Y(
-        "jitter:Q",
-        title=None,
-        axis=alt.Axis(values=[0], ticks=True, grid=False, labels=False), 
-        scale=alt.Scale(),
-        # sort=alt.EncodingSortField(field="count", order='descending')
-    ),    
-    x=alt.X("valenceScore:Q", scale=alt.Scale(domain=(0,1)),
-        color=alt.Color("valenceScore:Q", legend=None),
-        row=alt.Row(
-            "artistGenres:N",
-            # header=alt.Header(
-            #     labelAngle=0,
-            #     labelFontSize=16,
-            #     titleOrient='top',
-            #     labelOrient='lef',
-            #     labelAlign='left',
-            # )
-        ),
-    ),
+    y=alt.Y("artistGenres:N", axis=alt.Axis(title="Artist Genres"), sort=alt.EncodingSortField(field="count", order='descending')),    
+    x=alt.X("valenceScore:Q", axis=alt.Axis(title="Valence Score")),
+    # href='url:N',
+    tooltip=['trackName:N', "artistName:N", "artistGenres:N", "valenceScore:Q"],
+    # yOffset="jitter:Q",
+    # color=alt.Color("valenceScore:Q", legend=None, scale=alt.Scale(scheme='purples'))
 ).transform_calculate(
-    jitter="sqrt(-2*log(random()))*cos(2*PI*random())"
-)
+    # Generate an url to let people search for the tracks
+    #url='https://www.google.com/search?q=',
+    # Generate Gaussian jitter with a Box-Muller transform
+    jitter="sqrt(-2*log(random()))*sin(2*PI*random())"
+).properties(width=800, height=800)
 
 strip_plot_valence_artist_genres_all
