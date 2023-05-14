e84a3c35c502b778c3b79c00b917b5ef494bea0d
anwb <antonwb@live.dk>
2023-05-14 17:27:48 +0200

    12345555

--- stripPlotAll/stripPlotTopTenGenresDanceabilityAll.py
+++ stripPlotAll/stripPlotTopTenGenresDanceabilityAll.py
@@ -52,6 +52,6 @@ strip_plot_danceability_artist_genres_all = alt.Chart(df_genres_clean_10, width=
 ).transform_calculate(
     # Generate Gaussian jitter with a Box-Muller transform
     jitter='sqrt(-2*log(random()))*cos(2*PI*random())'
-).properties(width=5, height=40)
+).properties(width=1, height=40)
 
 strip_plot_danceability_artist_genres_all 
