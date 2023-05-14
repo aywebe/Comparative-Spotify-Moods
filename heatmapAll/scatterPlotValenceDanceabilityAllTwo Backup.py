9128c18a66b9e1606b125ec57bed211faf752b90
anwb <antonwb@live.dk>
2023-05-14 13:31:28 +0200

    minimap?????

--- heatmapAll/scatterPlotValenceDanceabilityAllTwo Backup.py
+++ heatmapAll/scatterPlotValenceDanceabilityAllTwo Backup.py
@@ -24,6 +24,14 @@ top_genres = df_genres_clean_2['artistGenres'].value_counts().nlargest(10).index
 # Filter the DataFrame to only include rows with one of the top 40 genres
 df_genres_clean_40 = df_genres_clean_2[df_genres_clean_2['artistGenres'].isin(top_genres)]
 
+# Create the strip plot using the filtered DataFrame
+scatter_plot_valence_danceability_all = alt.Chart(df_genres_clean_40).mark_circle(size=30, color="#6f82ac").encode(
+    y=alt.Y("danceability:Q", axis=alt.Axis(title="Danceability")),
+    x=alt.X("valenceScore:Q", axis=alt.Axis(title="Valence Score")),
+    tooltip=['trackName:N', "artistName:N", 'artistGenres:N','valenceScore:Q', 'danceability:Q', ],
+    # color=alt.Color("valenceScore:Q", legend=None, scale=alt.Scale(scheme='greys'))
+).properties(width=800, height=800)
+
 zoom = alt.selection_interval(encodings=["x", "y"])
 
 # Create the minimap using the entire dataset
@@ -41,23 +49,6 @@ minimap = (
         height=200,
         title="Minimap -- click and drag to zoom in the detail view",
     )
-)
-
-# Create the strip plot using the filtered DataFrame
-scatter_plot_valence_danceability_all = (
-    alt.Chart(df_genres_clean_40)
-    .mark_circle(size=30)
-    .encode(
-        x=alt.X(
-            "valenceScore:Q", scale=alt.Scale(domain={"selection": zoom.name, "encoding": "x"})
-        ),
-        y=alt.Y(
-            "danceability:Q",
-            scale=alt.Scale(domain={"selection": zoom.name, "encoding": "y"})
-        ),
-        color="#6f82ac",
-    )
-    .properties(width=800, height=800)
 )
 
 # Combine the scatter plot and the minimap
