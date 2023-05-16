7b51d3e670fd4c026f858d8bddc19f864aa2253c
anwb <antonwb@live.dk>
2023-05-16 16:08:52 +0200

    Update stripPlotTopTenGenresValenceAll.py

--- stripPlotAll/stripPlotTopTenGenresValenceAll.py
+++ stripPlotAll/stripPlotTopTenGenresValenceAll.py
@@ -38,7 +38,7 @@ strip_plot_valence_artist_genres_all =  alt.Chart(df_genres_clean_10, width=600,
         scale=alt.Scale(),
     ),
     x=alt.X('valenceScore:Q', title= "Valence Score", scale=alt.Scale(domain=(0, 1))),
-    color=alt.Color(legend=None),
+    color=alt.Color("artistGenres:N", legend=None),
     tooltip=["trackName", "artistName", "valenceScore"],
     row=alt.Row(
         'artistGenres:N',
